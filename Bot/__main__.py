2ea0362b6bddd633705ed967a58e003062830ee0
Don <96370825+SflixBots@users.noreply.github.com>
2022-04-14 13:44:47 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,1 +1,6 @@
+from Bot import Sflix
+from pyrogram import filters
+from pyrogram.types import Message
 
+@Sflix.on_message(filters.command("start") | filters. private)
+async def start(client: Sflix, message: Message):
