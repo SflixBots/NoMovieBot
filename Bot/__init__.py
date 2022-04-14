6afc77685ef11be55bdbbe4ec4aa4efa1e348755
Don <96370825+SflixBots@users.noreply.github.com>
2022-04-14 12:11:22 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,1 +1,9 @@
+from os import environ
 
+class Config:
+    #Config class for variables.
+
+    SESSION_NAME = "NoMovieBot"
+    API_ID = int(environ['API_ID'])
+    API_HASH = environ['API_HASH']
+    BOT_TOKEN = environ['BOT_TOKEN']
