88cf3cb15e781c2739bb191ce50a2b14dcdf7ee0
Don <96370825+SflixBots@users.noreply.github.com>
2022-04-14 13:28:00 +0530

    Update and rename run.py to bot_class.py

---
+++ bot_class.py
@@ -0,0 +1,23 @@
+from Bot import Config
+
+from pyrogram.raw.all import layer
+from pyrogram import Client, __version__
+
+SESSION = Config.SESSION_NAME
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+BOT_TOKEN = Config.BOT_TOKEN
+WORKERS = Config.WORKERS
+
+class Sflix(Client):
+    #Starts the Pyrogram Client on the Bot Token when we do 'python3 run.py'
+
+    def __init__(self):
+        super().__init__(
+           session_name=SESSION,
+           api_id=API_ID,
+           api_hash=API_HASH,
+           bot_token=BOT_TOKEN,
+           workers=WORKERS,
+           plugins={"root": "Bot"},
+        )
--- run.py
+++
@@ -1,4 +0,0 @@
-from Bot.bot_class import Sflix
-
-bot = Sflix()
-Sflix.run()
