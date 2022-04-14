c842fd098926461a7759f04f00b135b4187f9535
Don <96370825+SflixBots@users.noreply.github.com>
2022-04-14 20:35:46 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -10,4 +10,10 @@ class Config:
     BOT_TOKEN = environ['BOT_TOKEN']
     WORKERS = int(100)
 
+class script:
+    MOVIE_TXT = "Hello {}
+Looks like you are asking a movie
+But this is not a movie group! 👀
+Now you can leave from here or admins will kick you"
+
 Sflix = Client
