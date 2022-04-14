91c490bc9b77973cb131518ae77a4b8fe1b342a0
Don <96370825+SflixBots@users.noreply.github.com>
2022-04-14 12:38:33 +0530

    Update bot_class.py

--- Bot/bot_class.py
+++ Bot/bot_class.py
@@ -2,3 +2,11 @@ from Bot import Config
 
 from pyrogram.raw.all import layer
 from pyrogram import Client, __version__
+
+class Sflix(Client):
+    #Starts the Pyrogram Client on the Bot
+
+    def __init__(self):
+        super().__init__(
+            
+        )
