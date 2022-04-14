d1af4877a2adafc9633c67fe720f059ad352a6f0
Don <96370825+SflixBots@users.noreply.github.com>
2022-04-14 20:45:37 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -24,7 +24,7 @@ async def movie(client: Sflix, message: Message):
             InlineKeyboardButton("Ignore", callback_data="movie.ignore")
         ]]
         await message.reply_text(
-            text = script.MOVIE_TXT.format(reply_to.from_user.id),
+            text = script.MOVIE_TXT.format(message.from_user.mention),
             reply_markup = reply_markup,
             reply_to_message_id=reply_id
         )
