f1c21e5f102d11f85d7c07d03c17a5425be25074
Don <96370825+SflixBots@users.noreply.github.com>
2022-04-15 22:10:39 +0530

    Update utils.py

--- Bot/utils.py
+++ Bot/utils.py
@@ -1,1 +1,18 @@
+from imdb import IMDb
 
+imdb = IMDb()
+
+async def get_title(query):
+    query = (query.strip()).lower()
+    title = query
+    movieid = imdb.search_movie(title.lower(), results=10)
+    if not movieid:
+        return None
+    filtered = movieid
+    if not movieid:
+        movieid = filtered
+    movieid = movieid[0].movieID
+
+    movie = imdb.get_movie(movieid)
+
+    return {'poster': movie.get('full-size cover url')}
