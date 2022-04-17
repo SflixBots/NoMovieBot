8e5f6cd753865a3c2597262ff3c2dd1ad83cf4a9
Don <96370825+SflixBots@users.noreply.github.com>
2022-04-17 09:56:27 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -12,6 +12,27 @@ class Config:
     OMDB_KEY = environ.get("OMDB_KEY", "")
 
 class script:
+    START_TXT = """Hey There I'm {bot.first_name}
+
+**I'm a User-Friendly group Management Bot**
+
+⤜ **Awake Since:** `{uptime}`
+
+Click **Help Commands** to know how to use me."""
+
+    HELP_TXT = """Hi"""
+
+    ABOUT_TXT = """
+My name: Philip                
+                
+My creator: @Don_Sflix               
+                
+My language: Python               
+                
+My Library: Pyrogram 
+               
+My Source: <a href='https://github.com/SflixBots/NoMovieBot'>Click here</a>"""
+
     MOVIE_TXT = """Hello {}
 Looks like you are asking a movie
 But this is not a movie group! 👀
